8f917099a8bc57fc6f1df29313a36df263c601df
koke2511 <j.d.ferrero@hotmail.com>
2023-11-19 23:41:47 +0100

    Juego ajedrez

---
+++ src/Main/Main.java
@@ -0,0 +1,14 @@
+
+package Main;
+
+import Controlador.Controlador;
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+            Controlador controlador = new Controlador();
+    }
+}
